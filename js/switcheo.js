cdc87f1a414884be960a358b67cf83768e717e09
Akash Bansal <akash.bansal2504@gmail.com>
2018-08-15 17:12:18 +0530

    indent fix

--- js/switcheo.js
+++ js/switcheo.js
@@ -1,9 +1,13 @@
 'use strict';
+
 //  ---------------------------------------------------------------------------
 
 const Exchange = require ('./base/Exchange');
+
+//  ---------------------------------------------------------------------------
+
 module.exports = class switcheo extends Exchange {
-     describe () {
+    describe () {
         return this.deepExtend (super.describe (), {
             'id': 'switcheo',
             'name': 'Switcheo',
@@ -22,42 +26,50 @@ module.exports = class switcheo extends Exchange {
                 'fetchOpenOrders': true,
                 'fetchClosedOrders': true,
                 'withdraw': true,
-                'fetchFundingFees': true
+                'fetchFundingFees': true,
             },
-             'timeframes': {
+            'timeframes': {
                 '1m': '1m',
                 '5m': '5m',
                 '15m': '15m',
                 '30m': '30m',
                 '1h': '1h',
                 '6h': '6h',
-                '1d': '1d'
+                '1d': '1d',
             },
             'urls': {
                 'logo': 'https://docs.switcheo.network/images/logo.png',
                 'api': {
-                    'public': 'https://api.switcheo.network',
-                    'account': 'https://api.switcheo.network',
-                    'market': 'https://api.switcheo.network',
-                    'v2': 'https://api.switcheo.network/v2'
+                    'public': 'https://api.switcheo.network/v2/',
+                    'account': 'https://api.switcheo.network/v2/',
+                    'market': 'https://api.switcheo.network/v2/',
+                    'v2': 'https://api.switcheo.network/v2/',
                 },
                 'www': 'http://www.switcheo.exchange/',
                 'doc': [
-                    'https://docs.switcheo.network/'
+                    'https://docs.switcheo.network/',
                 ],
                 'fees': [
-                    'https://support.switcheo.network/switcheonetwork/switcheo-exchange-fee-structure/switcheo-exchange-fee-structure'
-                ]
+                    'https://support.switcheo.network/switcheonetwork/switcheo-exchange-fee-structure/switcheo-exchange-fee-structure',
+                ],
             },
             'api': {
-
+                'public': {
+                    'get': [
+                        'tickers/candlesticks',
+                        'tickers/last_24_hours',
+                        'tickers/last_price',
+                        'offers',
+                    ],
+                },
             },
             'fees': {
-
             },
             'exceptions': {
-
-            }
+            },
+            'options': {
+                'hasAlreadyAuthenticatedSuccessfully': false,
+            },
         });
-     }
-}
+    }
+};
